f1b92bab3e4d68cc71f411e7543799d74406e2b2
Udham Singh <92254626+udham8306@users.noreply.github.com>
2025-03-08 14:31:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your code here
+function changeText() {
+    document.getElementById("motivation").innerText = 
+        "Coding is like magic – with just a few lines, you can create something incredible. I want to keep learning and growing as a developer!";
+}
